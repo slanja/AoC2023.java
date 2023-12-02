957d38c36aca2ca9ba8fed77d68dc63b83cc8051
slanja <97552526+slanja@users.noreply.github.com>
2023-12-02 15:52:43 +0100

    functional puzzle02 part 2 (room to optimize and remove unnesecary code)

--- src/main/java/org/puzzles/Puzzle02.java
+++ src/main/java/org/puzzles/Puzzle02.java
@@ -12,24 +12,61 @@ import static java.lang.Character.isDigit;
 public class Puzzle02 {
     public static void main(String[] args) {
         int id = 1;
-        int num = 0;
-        int sum = 0;
+        int num = 0, sum = 0, power = 0;
 
         String[] split;
         try {
             List<String> allLines = Files.readAllLines(Paths.get("/home/slanja/IdeaProjects/AoC2023.java/src/main/java/org/puzzles/inputPuzzle02.txt"));
 
-
             for (String line : allLines) {
                 // spliting input file one value on each row
                 split = line.split(" ");
                 boolean possible = true;
+                int rHigh = 1, gHigh = 1, bHigh = 1;
 
                 for (int i = 0; i < split.length; i++) {
                     // removing unnecessary string, which is game and game id
                     if (split[i].contains("Game")) {
                         i += 2;
                     }
+
+                    // checking if value of specific color is highest one
+                    switch (i) {
+                        case 2:
+                        case 4:
+                        case 6:
+                        case 8:
+                        case 10:
+                        case 12:
+                        case 14:
+                        case 16:
+                        case 18:
+                        case 20:
+                        case 22:
+                        case 24:
+                        case 26:
+                        case 28:
+                        case 30:
+                        case 32:
+                        case 34:
+                        case 36:
+                        case 38:
+                        case 40:
+                            if (Integer.parseInt(split[i]) > rHigh && split[i+1].contains("red")) {
+                                rHigh = Integer.parseInt(split[i]);
+                                break;
+                            }
+                            else if (Integer.parseInt(split[i]) > gHigh && split[i+1].contains("green")) {
+                                gHigh = Integer.parseInt(split[i]);
+                                break;
+                            }
+
+                            else if (Integer.parseInt(split[i]) > bHigh && split[i+1].contains("blue")) {
+                                bHigh = Integer.parseInt(split[i]);
+                                break;
+                            }
+                    }
+
                     try {
                         num = Integer.parseInt(split[i]);
                     } catch (NumberFormatException e) {
@@ -48,11 +85,17 @@ public class Puzzle02 {
                     }
                 }
                 // adding up ids, when the game was possible
-                if (possible == true) sum += id;
+                if (possible) {
+                    sum += id;
+                }
+                // counting total amount of power
+                power += (rHigh * gHigh * bHigh);
+
                 id++;
-                // System.out.println(line);
             }
+            // printing results
             System.out.println("Sum: " + sum);
+            System.out.println("Power: " + power);
         } catch (IOException e) {
 
         }
