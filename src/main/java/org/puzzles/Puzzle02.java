38eb5b7efdef5836bd71642cb4496c1bf5e9436c
slanja <97552526+slanja@users.noreply.github.com>
2023-12-02 10:26:04 +0100

    puzzle02 part 1 working :)

--- src/main/java/org/puzzles/Puzzle02.java
+++ src/main/java/org/puzzles/Puzzle02.java
@@ -3,6 +3,7 @@ package org.puzzles;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.spec.RSAOtherPrimeInfo;
 import java.util.List;
 import java.util.Spliterator;
 
@@ -10,25 +11,48 @@ import static java.lang.Character.isDigit;
 
 public class Puzzle02 {
     public static void main(String[] args) {
-        // 12 red cubes, 13 green cubes, and 14 blue
         int id = 1;
-        int digit;
+        int num = 0;
+        int sum = 0;
+
         String[] split;
         try {
             List<String> allLines = Files.readAllLines(Paths.get("/home/slanja/IdeaProjects/AoC2023.java/src/main/java/org/puzzles/inputPuzzle02.txt"));
 
 
             for (String line : allLines) {
-                split = line.split(",");
-                for (int i = 0; i < split.length; i++) {
-                    if (split[i].contains("red")) {
+                // spliting input file one value on each row
+                split = line.split(" ");
+                boolean possible = true;
 
+                for (int i = 0; i < split.length; i++) {
+                    // removing unnecessary string, which is game and game id
+                    if (split[i].contains("Game")) {
+                        i += 2;
                     }
-                    System.out.println(id + ": " + split[i]);
+                    try {
+                        num = Integer.parseInt(split[i]);
+                    } catch (NumberFormatException e) {
+                        // 12 red cubes, 13 green cubes, and 14 blue
+
+                        // checking if the game is possible
+                        if (split[i].contains("red") && num > 12) {
+                            possible = false;
+                        }
+                        else if (split[i].contains("green") && num > 13) {
+                            possible = false;
+                        }
+                        else if (split[i].contains("blue") && num > 14) {
+                            possible = false;
+                        }
+                    }
                 }
+                // adding up ids, when the game was possible
+                if (possible == true) sum += id;
                 id++;
                 // System.out.println(line);
             }
+            System.out.println("Sum: " + sum);
         } catch (IOException e) {
 
         }
